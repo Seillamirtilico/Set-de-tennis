c515fe5a7e66aa70f7f9886067c1d6734ecb6855
Julian Fuentes <142046690+Seillamirtilico@users.noreply.github.com>
2023-08-17 15:02:53 -0500

    Add files via upload
    ya esta la estructura y ya esta gano b

--- Main.java
+++ Main.java
@@ -8,16 +8,30 @@ public class Main {
         int GanadosA = sc.nextInt();
         System.out.println("Ingresa los juegos ganados por el equipo B");
         int GanadosB = sc.nextInt();
-            // Si aun no se ha terminado esto
-            if (GanadosA<6){
-                if (GanadosB<6){
-                    System.out.println("El juego aun no termina");
-                }
-            // Si gana a esto
-            else if (GanadosA>=6) {
-                    System.out.println("aja");
-                }
-            }
-
+        int mas2 = GanadosA + 2;
+        //si aun no termina el juego
+        if ((GanadosA < 6) && (GanadosB < 6)) {
+            System.out.println("el juego aun no termina");
+        } else if ((GanadosB >=6) && (GanadosB>=mas2)) {
+            System.out.printf("gano el equipo b");
+        } else if (GanadosA == 3) {
+            // Código para la variableA igual a 3
+        } else if (GanadosA == 4) {
+            // Código para la variableB igual a 4
+        } else if (GanadosA == 5) {
+            // Código para la variableA igual a 5
+        } else if (GanadosA == 6) {
+            // Código para la variableA igual a 6
+        } else if (GanadosA == 7) {
+            // Código para la variableA igual a 7
+        } else if (GanadosA == 8) {
+            // Código para la variableB igual a 8
+        } else if (GanadosA == 9) {
+            // Código para la variableA igual a 9
+        } else if (GanadosA == 10) {
+            // Código para la variableA igual a 10
+        } else {
+            // Código si ninguna de las opciones anteriores coincide
+        }
     }
 }
