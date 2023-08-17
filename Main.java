cda134274270a79ac829235cc4eb7d13a97178d1
Julian Fuentes <142046690+Seillamirtilico@users.noreply.github.com>
2023-08-17 18:37:08 -0500

    Add files via upload
    El programa cumple con lo propuesto anque al poner resultados como 7-3 o demas dice que gano y no que es valido de resto funciona perfectamente

--- Main.java
+++ Main.java
@@ -3,35 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenido a tu programara de sets de tennis\n----------------------------------------- ");
+        System.out.println(      "\n" +
+                "               /xxxx\\\n" +
+                "              |xxxxxx|\n" +
+                "              |xxxxxx|      _\n" +
+                "              \\xxxxxx/     (~)\n" +
+                "               \\xxxx/\n" +
+                "                \\--/\n" +
+                "                 ||\n" +
+                "                 ||\n" +
+                "                 ||\n" +
+                "                 ||\n" +
+                "                 ||\n" +
+                "                 []\nBienvenido a tu programara de sets de tennis\n-------------------------------------------- ");
         System.out.println("Ingresa los juegos ganados por el equipo A");
         int GanadosA = sc.nextInt();
         System.out.println("Ingresa los juegos ganados por el equipo B");
         int GanadosB = sc.nextInt();
-        int mas2 = GanadosA + 2;
-        //si aun no termina el juego
-        if ((GanadosA < 6) && (GanadosB < 6)) {
+        int Paraganarb = GanadosA + 2;//((GanadosA < 6) && (GanadosB < 6))
+        int Paraganara = GanadosB + 2;
+        if ((GanadosA==5) && (GanadosB==5)) {
+            System.out.println("Empate 5 a 5");
+            //empate 5 a 5
+        } else if ((GanadosA ==6) && (GanadosB==6)){
+            System.out.println("Empate 6 a 6");
+            //empate 6 a 6
+        }
+        else if ((GanadosB >=6) && (GanadosB>=Paraganarb)) {
+            System.out.println("gano el equipo b");
+            // Gano el b
+        } else if ((GanadosA>=6) && (GanadosA>=Paraganara)) {
+            System.out.println("Gano el Equipo A");
+            // Gano el a
+        } else if((GanadosA <= 6) && (GanadosB <= 6))  {
             System.out.println("el juego aun no termina");
-        } else if ((GanadosB >=6) && (GanadosB>=mas2)) {
-            System.out.printf("gano el equipo b");
-        } else if (GanadosA == 3) {
-            // Código para la variableA igual a 3
-        } else if (GanadosA == 4) {
-            // Código para la variableB igual a 4
+            //EL juego no ha terminado
         } else if (GanadosA == 5) {
             // Código para la variableA igual a 5
-        } else if (GanadosA == 6) {
-            // Código para la variableA igual a 6
-        } else if (GanadosA == 7) {
-            // Código para la variableA igual a 7
-        } else if (GanadosA == 8) {
-            // Código para la variableB igual a 8
-        } else if (GanadosA == 9) {
-            // Código para la variableA igual a 9
-        } else if (GanadosA == 10) {
-            // Código para la variableA igual a 10
-        } else {
-            // Código si ninguna de las opciones anteriores coincide
+        }  else {
+            System.out.println("opcion invalida");
         }
     }
 }
